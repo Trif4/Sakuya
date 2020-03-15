7642bdb5ba1e7222da2a9de6a30c3819da321d1c
Trif <trif@live.dk>
2020-03-15 20:11:00 +0100

    Configure db migrations

---
+++ alembic/versions/e1a522588d35_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial revision
+
+Revision ID: e1a522588d35
+Revises: 
+Create Date: 2020-03-15 20:10:00.296872
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1a522588d35'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
